02cb40572c2e84e1b3aaaa1e6f5c1cdb8cce8ea6
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-08-22 20:30:10 -0300

    retomada do curso

---
+++ lacoIF.py
@@ -0,0 +1,23 @@
+#Laço If
+
+nome=input("Digite o nome: ")
+idade=int(input("Digite a idade: "))
+if idade>=65:
+    print("O paciente " + nome + " POSSUI atendimento prioritário!")
+else:
+    print("O paciente " + nome + " NÃO possui atendimento prioritário!")
+
+
+#Laço IF
+nome=input("Digite o nome: ")
+idade=int(input("Digite a idade: "))
+doenca_infectocontagiosa=input("Suspeita de doença infecto-contagiosa?").upper()
+if idade>=65 and doenca_infectocontagiosa=="SIM":
+    print("O paciente será direcionado para sala AMARELA -COM prioridade")
+elif idade < 65 and doenca_infectocontagiosa == "SIM":
+    print("O paciente será direcionado para sala AMARELA -SEM prioridade")
+elif idade >= 65 and doenca_infectocontagiosa == "NAO":
+    print("O paciente será direcionado para sala BRANCA -COM prioridade")
+elif idade < 65 and doenca_infectocontagiosa == "NAO":
+    print("O paciente será direcionado para sala BRANCA -SEM prioridade")
+else:print("Responda a suspeita de doença infectocontagiosa com SIM ou NAO")
