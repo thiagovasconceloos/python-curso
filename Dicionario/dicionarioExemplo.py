e206ea1bed36208933cc5af8de477f3625ee7a7d
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-08-23 18:57:35 -0300

    Continuando com o Curso

---
+++ Dicionario/dicionarioExemplo.py
@@ -0,0 +1,17 @@
+usuarios = {}
+
+
+usuarios = {"chaves:" : ["Chaves do 8","23/08/2022","Recep_01"],
+            "quico" : ["Quico das Flores","22/08/2022","Raio_01"]}
+
+print(usuarios)
+
+
+usuarios["Florinda"] = ["Dona Florinda","21/08/2022","Raio_01"]
+
+print(usuarios)
+
+
+print("#####--------####")
+
+print(usuarios.get("quico"))
