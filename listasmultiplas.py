f28e1424307440c6ec452197ff05864c531ae135
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-08-22 21:31:56 -0300

    Refatorando

---
+++ listasmultiplas.py
@@ -0,0 +1,26 @@
+equipamentos= []
+valores = []
+seriais = []
+departamentos = []
+resposta = "S"
+while resposta == "S":
+    equipamentos.append(input("Equipamento: "))
+    valores.append(float(input("Valor: ")))
+    seriais.append(int(input("Número Serial: ")))
+    departamentos.append(input("Departamento: "))
+    resposta = input("Digite \"S\"para continuar: ").upper()
+
+for indice in range(0,len(equipamentos)):
+    print("\nEquipamento..: ", (indice+1))
+    print("Nome.........: ", equipamentos[indice])
+    print("Valor........: ", valores[indice])
+    print("Serial.......: ", seriais[indice])
+    print("Departamento.: ", departamentos[indice])
+
+
+buscar = input("Digite o nome do Equipamento que deseja buscar: ")
+
+for indice in range(0,len(equipamentos)):
+    if buscar==equipamentos[indice]:
+        print("Valor: ",valores[indice])
+        print("Serial: ", seriais[indice])
